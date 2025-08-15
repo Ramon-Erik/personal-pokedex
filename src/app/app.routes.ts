78c44448a953696d0ffafe08ae6ff60ae7bd94fb
ramon-erik <ramonerik29a@gmail.com>
2025-08-15 15:27:33 -0300

    rota home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Inicio',
+        loadComponent: () => import('./modules/home/home').then(p => p.Home)
+    }
+];
