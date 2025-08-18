87e719d9a4043ce1b402939e88691fea23b825db
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-18 15:36:52 -0300

    feat: created Pokemon list component

---
+++ src/app/shared/components/pokemon-list/pokemon-list.ts
@@ -0,0 +1,21 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { PokeApi } from '../../../core/pokedex/poke-api';
+import { PokemonItem } from "../pokemon-item/pokemon-item";
+
+@Component({
+  selector: 'app-pokemon-list',
+  imports: [PokemonItem],
+  templateUrl: './pokemon-list.html',
+  styleUrl: './pokemon-list.scss',
+})
+export class PokemonList implements OnInit {
+  // @Input() filters: null
+
+  #pokeApiService = inject(PokeApi)
+
+  public pokemonList = this.#pokeApiService.getPokemonList
+
+  ngOnInit(): void {
+    this.#pokeApiService.httpPokemonList$(null).subscribe()
+  }
+}
