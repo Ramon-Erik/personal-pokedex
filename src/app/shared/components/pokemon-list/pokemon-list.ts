254491ebe3243c5a647927ba666d38ebed1993d0
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-21 18:28:45 -0300

    feat: filtering and paginating

--- src/app/shared/components/pokemon-list/pokemon-list.ts
+++ src/app/shared/components/pokemon-list/pokemon-list.ts
@@ -1,31 +1,70 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  Input,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { PokeApi } from '../../../core/pokedex/poke-api';
-import { PokemonItem } from "../pokemon-item/pokemon-item";
+import { PokemonItem } from '../pokemon-item/pokemon-item';
+import { Pagination } from '../pagination/pagination';
+import { IFilters } from '../../interface/filters';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list',
-  imports: [PokemonItem],
+  imports: [PokemonItem, Pagination],
   templateUrl: './pokemon-list.html',
   styleUrl: './pokemon-list.scss',
 })
 export class PokemonList implements OnInit {
-  // @Input() filters: null
+  #pokeApiService = inject(PokeApi);
+  public pokemonList = this.#pokeApiService.getPokemonList;
 
-  #pokeApiService = inject(PokeApi)
+  public itemsPerPage = 20;
+  public currentPage = 1;
 
-  public pokemonList = this.#pokeApiService.getPokemonList
-
-  public next() {
-    console.log('next');
-
-  }
-
-  public prev() {
-    console.log('prev');
+  /**
+   * name
+   */
+  public name() {
     
   }
 
+  public listFilters = signal<IFilters>({
+    id: '',
+    type: '',
+    weakness: '',
+    ability: '',
+    height: '',
+    weight: '',
+  });
+  @Input({ required: true }) set selectedFilters(filters: IFilters) {
+    this.listFilters.set(filters);
+  }
+
+  public filteredPokemonList = computed(() => {
+    const list = this.pokemonList();
+    const filters = this.listFilters();
+
+    if (!list) {
+      return [];
+    }
+    return list.filter((pokemon) => {
+      if (filters.id && !pokemon.name.includes(filters.id)) return false
+      // if (filters.id && !pokemon.id.toString().includes(filters.id)) return false
+      return true
+    });
+  });
+
+  public slicedList = computed(() => {
+    const start = (this.currentPage - 1) * this.itemsPerPage
+    const end = start + this.itemsPerPage
+    return this.filteredPokemonList().slice(start, end)
+  })
+
   ngOnInit(): void {
-    this.#pokeApiService.httpPokemonList$(null).subscribe()
+    this.#pokeApiService.httpPokemonList$(null).subscribe();
   }
 }
