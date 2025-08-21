6494ba7fd4e23e83aeb97dab57992875f2878d13
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-21 18:27:02 -0300

    feat: filters forms on home

--- src/app/modules/home/home.ts
+++ src/app/modules/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { PokeApi } from '../../core/pokedex/poke-api';
 import { PokemonList } from '../../shared/components/pokemon-list/pokemon-list';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [PokemonList],
+  imports: [PokemonList, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
@@ -12,7 +13,24 @@ export class Home implements OnInit {
   #pokeApiService = inject(PokeApi)
   public typesList = this.#pokeApiService.getTypesList
   public abilitiesList = this.#pokeApiService.getAbilitiesList
-  public filters = signal(null)
+  public filters = signal({
+    id: '',
+    type: "invalid",
+    weakness: "invalid",
+    ability: "invalid",
+    height: "invalid",
+    weight: "invalid"
+  })
+
+  public changeListFilters(pokeId: string, type: string, weakness: string, ability: string, height: string, weight: string,) {
+    this.filters.set({id: pokeId,
+type,
+weakness,
+ability,
+height,
+weight});
+    
+  }
 
   ngOnInit(): void {
     this.#pokeApiService.httpTypesList$().subscribe()
