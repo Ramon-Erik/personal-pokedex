08bc2fe29eff4614fb67b8ae6a7252fd99b3050e
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-25 19:40:21 -0300

    removed abilities and types search

--- src/app/core/pokedex/poke-api.ts
+++ src/app/core/pokedex/poke-api.ts
@@ -10,28 +10,6 @@ export class PokeApi {
   #http = inject(HttpClient);
   #url = 'https://pokeapi.co/api/v2';
 
-  #setTypesList = signal<IPokemonApiRequest | null>(null);
-  get getTypesList() {
-    return this.#setTypesList.asReadonly();
-  }
-  public httpTypesList$(): Observable<IPokemonApiRequest> {
-    this.#setTypesList.set(null);
-    return this.#http.get<IPokemonApiRequest>(`${this.#url}/type/`).pipe(
-      tap(res => this.#setTypesList.set(res))
-    );
-  }
-
-  #setAbilitiesList = signal<IPokemonApiRequest | null>(null);
-  get getAbilitiesList() {
-    return this.#setAbilitiesList.asReadonly();
-  }
-  public httpAbilitiesList$(): Observable<IPokemonApiRequest> {
-    this.#setAbilitiesList.set(null);
-    return this.#http.get<IPokemonApiRequest>(`${this.#url}/ability/`).pipe(
-      tap((res) => this.#setAbilitiesList.set(res))
-    );
-  }
-
   #setPokemonList = signal<Array<any>>([]);
   get getPokemonList() {
     return this.#setPokemonList.asReadonly();
