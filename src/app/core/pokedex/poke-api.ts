e3aecb8ee7fc6939526d1fafd3ec059292d1a621
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-25 20:15:28 -0300

    feat: suporte para load more function

--- src/app/core/pokedex/poke-api.ts
+++ src/app/core/pokedex/poke-api.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { BehaviorSubject, forkJoin, Observable, shareReplay, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  forkJoin,
+  switchMap,
+} from 'rxjs';
 import { IPokemonApiRequest } from '../../shared/interface/pokemon-types-list';
 
 @Injectable({
@@ -10,17 +14,28 @@ export class PokeApi {
   #http = inject(HttpClient);
   #url = 'https://pokeapi.co/api/v2';
 
-  private readonly pokemonListSubject$ = new BehaviorSubject<Array<any>>([])
-  public readonly pokemonList$ = this.pokemonListSubject$.asObservable()
-  public fetchPokemonList(range: {offset: number, limit: number}) {
-    const url = `${this.#url}/pokemon?offset=${range.offset}&limit=${range.limit}`
+  private readonly pokemonListSubject$ = new BehaviorSubject<Array<any>>([]);
+  public readonly pokemonList$ = this.pokemonListSubject$.asObservable();
+  public fetchPokemonList(range: { offset: number; limit: number }) {
+    const url = `${this.#url}/pokemon?offset=${range.offset}&limit=${
+      range.limit
+    }`;
 
-    return this.#http.get<IPokemonApiRequest>(url).pipe(
-      switchMap(res => {
-        const requests = res.results.map(pokemon => this.#http.get<{}>(pokemon.url)) 
-        return forkJoin(requests)
-      }),
-      tap(pokemonInfo => this.pokemonListSubject$.next(pokemonInfo))
-    )
+    this.#http
+      .get<IPokemonApiRequest>(url)
+      .pipe(
+        switchMap((res) =>
+          forkJoin(
+            res.results.map((pokemon) => this.#http.get<{}>(pokemon.url))
+          )
+        )
+      )
+      .subscribe({
+        next: (pokemonInfo) => {
+          const currentList = this.pokemonListSubject$.getValue();
+          this.pokemonListSubject$.next([...currentList, ...pokemonInfo]);
+        },
+        error: (erro) => console.log(erro),
+      });
   }
 }
