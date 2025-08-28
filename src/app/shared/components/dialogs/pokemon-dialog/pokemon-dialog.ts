6f2642350d823afc45e911f4381e89b743b1705f
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-28 17:04:10 -0300

    feat: using progress bar on pokemon stats

--- src/app/shared/components/dialogs/pokemon-dialog/pokemon-dialog.ts
+++ src/app/shared/components/dialogs/pokemon-dialog/pokemon-dialog.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, OnInit, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { IPokemon } from '../../../interface/pokemon-item.interface';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-pokemon-dialog',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatProgressBarModule],
   templateUrl: './pokemon-dialog.html',
   styleUrl: './pokemon-dialog.scss'
 })
