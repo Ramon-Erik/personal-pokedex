98beaed5335ce0facd3219f895ab467da52b26b5
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-09-04 16:06:48 -0300

    feat: created filter selects

--- src/app/modules/home/home.ts
+++ src/app/modules/home/home.ts
@@ -5,10 +5,33 @@ import { PokemonList } from '../../shared/components/pokemon-list/pokemon-list';
   selector: 'app-home',
   imports: [PokemonList],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
   public setFilter(text: string) {
-    return text.toLowerCase()
+    return text.toLowerCase();
   }
+
+  public hideTypes = signal(false)
+
+  public pokemonTypes = [
+    'Normal',
+    'Fire',
+    'Water',
+    'Electric',
+    'Grass',
+    'Ice',
+    'Fighting',
+    'Poison',
+    'Ground',
+    'Flying',
+    'Psychic',
+    'Bug',
+    'Rock',
+    'Ghost',
+    'Dragon',
+    'Dark',
+    'Steel',
+    'Fairy',
+  ];
 }
